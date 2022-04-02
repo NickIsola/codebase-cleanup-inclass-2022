625a80a399f337ab7c5bf1cf638f04b76dfc04d7
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-04-02 18:42:33 -0400

    Test price formatting logic (#5)
    * Test price formatting logic
    
    * Testing Revisions
    
    Made more dynamic and inclusive

---
+++ tests/utils_test.py
@@ -0,0 +1,9 @@
+# this is the "tests/utils_test.py" file...
+
+from app.utils import to_usd
+
+def test_to_usd():
+    # rounds to two decomal places and have a dollar sign
+    # if large number should use thousands separator
+    assert to_usd(1234.79) == "$1,234.79"
+
