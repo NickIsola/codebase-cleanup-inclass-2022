a76e11a91fa6feacd46558dd8748e752cfec1ec9
NickIsola <77946751+NickIsola@users.noreply.github.com>
2022-04-05 18:25:53 -0400

    Update groceries.py

--- app/groceries.py
+++ app/groceries.py
@@ -9,13 +9,9 @@ import os
 
 def to_usd(my_price):
     """
-    This is a docstring. It tells us what this function is about. 
-    What its responsibilites are
-    What the params are about
-    What datatypes the params are
-    What this funciton will return
-    Example of invoking the function
-
+    takes in an float number value,
+    returns a string with an appended $ sign 
+    and number formatted to 2 decimal places
     Invoke like this: to_usd(9.999)
     """
     return '${:,.2f}'.format(my_price)
